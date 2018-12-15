933a3b60a35bf45e2e9b4e40d85b0e4df289be06
Chinedu Ebinim <39536030+neydoo@users.noreply.github.com>
2018-12-15 12:38:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -28,15 +28,15 @@ app.use(bodyParser.json())
 app.use(logger('dev'))
 app.use(errorhandler())
 app.use((req, res, next) => {
-    req.store = store
+    req.store = store  
     next()
 })
-
+// postHandler
 app.get('/posts', routes.posts.getPosts)
 app.post('/posts', routes.posts.addPost)
 app.put('/posts/:postId', routes.posts.updatePost)
 app.delete('/posts/:postId', routes.posts.removePost)
-
+// commentHandler
 app.get('/posts/:postId/comments', routes.comments.getComments)
 app.post('/posts/:postId/comments', routes.comments.addComment)
 app.put('/posts/:postId/comments/:commentId', routes.comments.updateComment)
